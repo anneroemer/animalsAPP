b82b68ecc88878d02b411ee2d57791d9fd4dd862
anneroemer <77618343+anneroemer@users.noreply.github.com>
2021-06-09 08:27:52 +0200

    Added files

---
+++ delete.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    // const deleteBtns = document.querySelectorAll(".deleteBtn");
+    // console.log(deleteBtns);
+
+    // const form = new FormData();
+    // let url = new URLSearchParams(window.location.search);
+    // let animalID = url.get("animalId");
+
+
+    // deleteBtns.forEach(deleteBtn =>{
+    //     console.log(deleteBtn);
+    //     deleteBtn.addEventListener("click", function(){
+    //         fetch(`http://annes-api.herokuapp.com/api/v1/animals/${animalID}`, {
+    //         "method": "DELETE",
+    //         "headers": {
+    //             "Content-Type": "multipart/form-data"
+    //         },
+    //         "body": form
+    //         })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data)
+    //         })
+    //     })
+    // })
+    
+})
---
+++ goback.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    // let goBackElm = document.querySelector(".single-animal__back-btn");
+
+    // if(goBackElm){
+    //     goBackElm.addEventListener("click", function(){
+    //         history.back();
+    //     })
+    // }
+})
